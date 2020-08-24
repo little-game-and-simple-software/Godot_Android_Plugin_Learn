692625521835be7608df863f1f0c9c1601f6f01b
2439905184 <suchenglong018@126.com>
2020-08-24 09:29:24 +0800

    添加了映射插件的方法 添加了public View onMainCreate方法

--- GodotHelloPlugin/src/main/java/com/hh128/GodotHelloPlugin/HelloGodot.java
+++ GodotHelloPlugin/src/main/java/com/hh128/GodotHelloPlugin/HelloGodot.java
@@ -1,28 +1,44 @@
 package com.hh128.GodotHelloPlugin;
 
 import android.app.Activity;
-import android.content.Context;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.collection.ArraySet;
 
+
 import org.godotengine.godot.Godot;
 import org.godotengine.godot.plugin.GodotPlugin;
 import org.godotengine.godot.plugin.SignalInfo;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
-
 public class HelloGodot extends GodotPlugin
 {
     //游戏主activity
-    private Activity activity=null;
-
+    public Activity activity=null;
+    public FrameLayout layout=null;
     //Godot是继承自activity的类 从这里获得activity实例
     public HelloGodot(Godot godot)
     {
         super(godot);
         activity=godot;
+    }
+    public View onMainCreate(Activity activity2)
+    {
+        this.layout=new FrameLayout(activity);
+        return this.layout;
+    }
+
+    //返回给godot调用的方法名称 用于映射和架桥
+    @NonNull
+    @Override
+    public List<String> getPluginMethods()
+    {
+        return Arrays.asList(new String[]{"test"});
     }
 
     @NonNull
